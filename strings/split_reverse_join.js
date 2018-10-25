523324ac0ed710bd08e608a4db493463fd09599b
rafcevallos <rafael.cevallos81@gmail.com>
2018-10-25 16:04:00 -0500

    Started String Method Exercise

---
+++ strings/split_reverse_join.js
@@ -0,0 +1,27 @@
+/*
+TODO:
+
+TASK
+
+Complete the reverseString function; it has one parameter, s. You must perform the following actions:
+
+1. Try to reverse string s using the split, reverse, and join methods.
+
+2. If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+
+3. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+
+OUTPUT FORMAT
+
+You must write two print statements using console.log():
+
+    1. Print the contents of a caught exception's message on a new line. If no exception was thrown, this line should not be printed.
+
+    2. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+*/
+
+function reverseString(s) {
+    
+}
